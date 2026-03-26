9f07269a5374540f02669703328f9a1eeb2ee768
Emanuel Pereyra <emanuelmetal@gmail.com>
2026-03-26 20:15:36 -0300

    rollback to next-themes and add cookies

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -1,7 +1,12 @@
-'use server'; // Mark function as a Server Action
+'use server';
 import { cookies } from 'next/headers';
 
 export async function setCookieValue(key: string, value: string) {
   const cookieStore = await cookies();
   cookieStore.set(key, value, { path: '/', maxAge: 60 * 60 * 24 * 365 }); // Set the cookie for 1 year
+}
+
+export async function getCookieValue(key: string) {
+  const cookieStore = await cookies();
+  return cookieStore.get(key)?.value || null;
 }
